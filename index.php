fa71a0d9018fba79d3e7f2edd3076360ad370779
nhanvtgcd191366 <84842522+nhanvtgcd191366@users.noreply.github.com>
2021-06-14 10:45:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <html>
 <head>
-<title>PHP Test</title>
+<title> Database Test </title>
 </head>
 <body>
-<?php echo '<p>Hello World</p>'; ?>
+<?php
+include("connection.php");
+//Connect
+$pg_conn = pg_connect($conn_string);
+#Get data
+$result = pg_query($pg_conn, "select * from product;");
+?>
+<?php
+	include("db_display.php");
+	display_table($result);
+	pg_close();
+?>
+</table>
 </body>
 </html>
